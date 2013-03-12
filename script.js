b4cf90dd7c249830937f2ce3840d5533cd4322f6
Said <saboukharroub@lri-lb.org>
2013-03-12 14:39:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $('#map').mapbox('lri.StaffDirectory', function(map, tilejson) {
     mapbox.share().map(map).add();
 
     // Set title and description from tilejson
-    document.title = "LRI Staff Directory";
+    document.title = title;
     $('h1.map-title').text(tilejson.name);
     $('p.description').text(tilejson.description);
 
